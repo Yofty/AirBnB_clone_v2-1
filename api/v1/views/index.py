5362699ca1d9c025521aa2f949e2558f26a7347c
Yofty <106748943+Yofty@users.noreply.github.com>
2023-04-06 06:24:36 -0700

    crrection task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,7 +13,7 @@ def status():
     """
     Returns a JSON status
     """
-    return jsonify({"status": "OK"})
+    return jsonify(status= "OK")
 
 
 @app_views.route('/stats', strict_slashes=False)
@@ -21,9 +21,11 @@ def count():
     """
     Retrieves the number of each objects by type
     """
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+    return jsonify(
+        amenities=storage.count("Amenity"),
+        cities=storage.count("City"),
+        places=storage.count("Place"),
+        reviews=storage.count("Review"),
+        states=storage.count("State"),
+        users=storage.count("User")
+    )
