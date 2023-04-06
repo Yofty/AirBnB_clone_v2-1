d446d4ea3dbd43dfe6dcfefaaf809be7cc0763a2
Yofty <106748943+Yofty@users.noreply.github.com>
2023-04-06 07:20:49 -0700

    doucmentation added

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Contains the cities view for the API.'''
+"""Contains the cities view for the API."""
 from flask import abort, jsonify, make_response, request
 from api.v1.views import app_views
 from models import storage
@@ -10,7 +10,7 @@ from models.state import State
 @app_views.route('/states/<state_id>/cities',
                  methods=['GET'], strict_slashes=False)
 def cities(state_id):
-    """Retrieves the list of all City objects of a State"""
+    """Retrieves the list of all City objects of a State."""
     obj_state = storage.get(State, state_id)
     if not obj_state:
         abort(404)
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""State module"""
+"""
+State module
+     - lists all the states.
+"""
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
